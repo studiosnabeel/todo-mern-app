daf78f0669a7542e6006c9aecebbbfffb0c82e54
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-18 01:41:32 +0500

    Add todo form

--- client/src/components/TodoForm.jsx
+++ client/src/components/TodoForm.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoForm = () => {
+  const [text, setText] = useState('');
+  const handleSubmit = () => {};
+  const handleChange = (e) => {
+    e.preventDefault();
+    setText(e.target.value);
+  };
   return (
-    <form className="form-container">
-      <input className="form-input" type="text" placeholder="Add tasks" />
+    <form className="form-container" onSubmit={handleSubmit}>
+      <input
+        className="form-input"
+        type="text"
+        placeholder="Add tasks"
+        onChange={handleChange}
+      />
     </form>
   );
 };
