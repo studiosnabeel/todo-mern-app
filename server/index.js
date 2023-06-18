7ff253f813f748db52a7850e81f1c52e08498f2e
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-18 16:23:43 +0500

    node modules remove

--- server/index.js
+++ server/index.js
@@ -4,4 +4,6 @@ const app = express();
 
 const PORT = 8000;
 
-app.listen(PORT, () => console.log(`server running successfully at PORT ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`server running successfully at PORT ${PORT}`)
+);
