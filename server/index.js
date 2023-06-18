3ffbb3b26ae09ddd4009255798f80d2918bf2c06
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-18 18:14:46 +0500

    call connection function in index.js file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import connection from './database/db.js';
 
 const app = express();
 
 const PORT = 8000;
+
+connection();
 
 app.listen(PORT, () =>
   console.log(`server running successfully at PORT ${PORT}`)
