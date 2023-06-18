46fbf86aeb9d587203a51db8e0d718e659405281
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-18 15:40:39 +0500

    Connect express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,7 @@
- import express
+import express from 'express';
+
+const app = express();
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log(`server running successfully at PORT ${PORT}`));
